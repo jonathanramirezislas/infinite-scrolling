a418565361da74714363045dea05990445a29a87
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-02-22 22:48:39 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Counter } from './components/Counter';
+
+function App() {
+  return (
+    <>
+      <h1>React + TypeScript</h1>
+      <hr/>
+      <h2>Hi!!</h2>
+
+      <Counter/>
+
+    </>
+  );
+}
+
+export default App;
